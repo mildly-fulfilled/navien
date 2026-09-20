e2836a6f4aca8fbef24d66d3ae1c94947062d108
mildly-fulfilled <124628668+mildly-fulfilled@users.noreply.github.com>
2026-09-20 18:05:19 -0400

    Update navien_link.h

--- esphome/components/navien/navien_link.h
+++ esphome/components/navien/navien_link.h
@@ -136,6 +136,7 @@ public:
   void send_scheduled_recirculation_on_cmd();
   void send_scheduled_recirculation_off_cmd();
   void send_dhw_set_temp_cmd(float temp);
+  void send_sh_set_temp_cmd(float temp);
 
   
 public:
